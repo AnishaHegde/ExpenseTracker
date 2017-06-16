6f6c2fb59b232ab1cf0dd3df19892328a57c7e99
AnishaHegde <anishajayakar.hegde@sjsu.edu>
2017-06-16 10:09:18 -0700

    changed configuration

--- routes/mysql.js
+++ routes/mysql.js
@@ -6,7 +6,7 @@ var pool = mysql.createPool({
 	  connectionLimit : 1000,
 	  host            : 'localhost',
 	  user            : 'root',
-	  password        : 'root',
+	  password        : '',
 	  database		  :	'expensetrackerdb',
 	  port			  :  3306
 	});
